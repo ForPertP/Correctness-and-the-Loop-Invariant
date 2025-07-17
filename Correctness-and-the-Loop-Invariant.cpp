2a2ce7b8421da9cf82e255529d079dee9e7ac7ae
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-18 06:14:15 +0900

    Update Correctness-and-the-Loop-Invariant.cpp

--- Correctness-and-the-Loop-Invariant.cpp
+++ Correctness-and-the-Loop-Invariant.cpp
@@ -1,117 +1,72 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-
 #include <bits/stdc++.h>
-
 using namespace std;
 
 string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
 
-/*
- * Complete the 'insertionSort2' function below.
- *
- * The function accepts following parameters:
- *  1. INTEGER n
- *  2. INTEGER_ARRAY arr
- */
 template <typename T>
 void show_value(const T& arr)
 {
     for (auto element : arr)
-    {
         cout << element << ' ';
-    }
-    
     cout << '\n';
 }
 
-
-void insertionSort(int n, vector<int> arr)
+void insertionSort2(int n, vector<int> arr)
 {
     for (int i = 1; i < n; ++i)
     {
         int key = arr[i];
-        int j;
-
-        for (j = i-1; j >= 0 && arr[j] > key; --j)
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
         {
-            arr[j+1] = arr[j];
+            arr[j + 1] = arr[j];
+            --j;
         }
-
-        arr[j+1] = key;
+        arr[j + 1] = key;
+        show_value(arr);
     }
-    
-    show_value(arr);
 }
 
 int main()
 {
     string n_temp;
     getline(cin, n_temp);
-
     int n = stoi(ltrim(rtrim(n_temp)));
 
     string arr_temp_temp;
     getline(cin, arr_temp_temp);
-
     vector<string> arr_temp = split(rtrim(arr_temp_temp));
 
     vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        arr[i] = stoi(arr_temp[i]);
 
-    for (int i = 0; i < n; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
-        arr[i] = arr_item;
-    }
-
-    insertionSort(n, arr);
-
+    insertionSort2(n, arr);
     return 0;
 }
 
 string ltrim(const string &str) {
     string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
+    s.erase(s.begin(), find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
     return s;
 }
 
 string rtrim(const string &str) {
     string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
+    s.erase(find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(), s.end());
     return s;
 }
 
 vector<string> split(const string &str) {
     vector<string> tokens;
-
     string::size_type start = 0;
     string::size_type end = 0;
-
     while ((end = str.find(" ", start)) != string::npos) {
         tokens.push_back(str.substr(start, end - start));
-
         start = end + 1;
     }
-
     tokens.push_back(str.substr(start));
-
     return tokens;
 }
-
