c401f53eb757600c86372d27960a223415b25a5c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-20 08:52:41 +0900

    Update Correctness-and-the-Loop-Invariant.cs

--- Correctness-and-the-Loop-Invariant.cs
+++ Correctness-and-the-Loop-Invariant.cs
@@ -1,1 +1,21 @@
-Correctness-and-the-Loop-Invariant
+using System;
+using System.Linq;
+
+class Solution
+{
+    static void InsertionSort2(int n, int[] arr)
+    {
+        for (int i = 1; i < n; ++i)
+        {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        ShowArray(arr);
+    }
