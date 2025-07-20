cdcab859891f7463900310d7140070ca46d5fb56
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-21 06:43:36 +0900

    Update Correctness-and-the-Loop-Invariant.java

--- Correctness-and-the-Loop-Invariant.java
+++ Correctness-and-the-Loop-Invariant.java
@@ -15,3 +15,22 @@ public class Solution {
 
         showArray(arr);
     }
+
+    public static void showArray(int[] arr) {
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        String[] arrStr = scanner.nextLine().trim().split("\\s+");
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(arrStr[i]);
+
+        insertionSort2(n, arr);
+    }
+}
